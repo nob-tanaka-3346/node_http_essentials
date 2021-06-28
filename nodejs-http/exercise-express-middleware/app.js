fa1b23a5931719012564db2c1810f0fba25a00c8
nobtanaka <nobtanaka3346@hotmail.co.jp>
2021-06-28 11:23:00 +0900

    edited

--- nodejs-http/exercise-express-middleware/app.js
+++ nodejs-http/exercise-express-middleware/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
+
+function isAuthorized(req,res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'secretpassword') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
+
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/users", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      name: "User Userson",
-    },
-  ]);
-});
+app.get('/users', isAuthorized, (req,res) => {
+  res.json([{
+    id: 1,
+    name: 'User Userson'
+  }])
+ })
 
 app.get("/products", (req, res) => {
   res.json([
